a76a61d5d6248b16404d935cb62cd4b455e74c69
Mohammad Amin Sameti <mamins1376@gmail.com>
2017-02-21 19:31:14 +0330

    reindent to 4 spaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-# vim: fenc=utf-8 ts=2 et sw=2 sts=2
+# vim: fenc=utf-8 ts=4 et sw=4 sts=4
 
 # This file is part of Subscene-API.
 #
@@ -26,32 +26,32 @@ from setuptools import setup, find_packages
 description = readme = 'python wrapper for Subscene subtitle database.'
 requirements = []
 try:
-  with open('README','r') as f:
-    readme = f.read()
-  with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+    with open('README','r') as f:
+        readme = f.read()
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
 except Exception:
-  pass
+    pass
 
 setup(
-  name='subscene-api',
-  version='1.0.0',
-  description=description,
-  long_description=readme,
-  author='Mohammad Amin Sameti',
-  author_email='mamins1376@gmail.com',
-  url='https://github.com/mamins1376/Subscene-API',
-  license='GNU General Public License v3 or later',
-  keywords = 'subscene api wrapper python3',
-  install_requires=requirements,
-  packages=['subscene'],
-  classifiers = [
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    'Topic :: Software Development',
-    'Topic :: Internet :: WWW/HTTP',
-    'Programming Language :: Python :: 3',
-    'Operating System :: OS Independent',
-    'Development Status :: 5 - Production/Stable'
-    ]
+    name='subscene-api',
+    version='1.0.0',
+    description=description,
+    long_description=readme,
+    author='Mohammad Amin Sameti',
+    author_email='mamins1376@gmail.com',
+    url='https://github.com/mamins1376/Subscene-API',
+    license='GNU General Public License v3 or later',
+    keywords = 'subscene api wrapper python3',
+    install_requires=requirements,
+    packages=['subscene'],
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Software Development',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable'
+        ]
 )
 
